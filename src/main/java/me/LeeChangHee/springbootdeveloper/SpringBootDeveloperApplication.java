83171250c2e26c38f856cef6d1eeac299d218633
2WindowLight <lch6648850@gnail.com>
2024-03-14 19:39:15 +0900

    init

---
+++ src/main/java/me/LeeChangHee/springbootdeveloper/SpringBootDeveloperApplication.java
@@ -0,0 +1,12 @@
+package me.LeeChangHee.springbootdeveloper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.*;
+
+@SpringBootApplication
+public class SpringBootDeveloperApplication {
+    public static void main(String[] args){
+        run(SpringBootDeveloperApplication.class,args);
+    }
+}
